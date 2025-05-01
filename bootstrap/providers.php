61f8550e34814a5bf2bf7779df24cb90e3d5f67a
Repwise <teddyoweh@gmail.com>
2025-05-02 02:33:00 +0400

    Add Documentation module with routing, service provider, and markdown files

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -10,4 +10,5 @@ return [
     App\Providers\HorizonServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
     App\Providers\MacroServiceProvider::class,
+    Repwise\Documentation\DocumentationServiceProvider::class,
 ];
