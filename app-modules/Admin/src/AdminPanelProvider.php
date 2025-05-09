28d2f8ea7951a730d7f6e038c16c3e6803a713be
Repwise <teddyoweh@gmail.com>
2025-05-09 16:12:58 +0400

    Refactor admin module: update resource paths and remove unnecessary declarations in team resource pages

--- app-modules/Admin/src/AdminPanelProvider.php
+++ app-modules/Admin/src/AdminPanelProvider.php
@@ -39,9 +39,8 @@ final class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Amber,
             ])
             ->brandName('Repwise Admin')
-            ->favicon('images/logo.png')
-            ->discoverResources(in: app_path('Filament/App/Resources'), for: 'Repwise\\Admin\\Filament\\Resources')
-            ->discoverPages(in: app_path('Filament/App/Pages'), for: 'Repwise\\Admin\\Filament\\Pages')
+            ->discoverResources(in: base_path('app-modules/Admin/src/Filament/Resources'), for: 'Repwise\\Admin\\Filament\\Resources')
+            ->discoverPages(in: base_path('app-modules/Admin/src/Filament/Pages'), for: 'Repwise\\Admin\\Filament\\Pages')
             ->navigationGroups([
                 NavigationGroup::make()
                     ->label('User Management'),
@@ -59,7 +58,7 @@ final class AdminPanelProvider extends PanelProvider
             ->pages([
                 Dashboard::class,
             ])
-            ->discoverWidgets(in: app_path('Filament/App/Widgets'), for: 'Repwise\\Admin\\Filament\\Widgets')
+            ->discoverWidgets(in: base_path('app-modules/Admin/src/Filament/Widgets'), for: 'Repwise\\Admin\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
                 OverlookWidget::class,
--- app-modules/Admin/src/Filament/Resources/TeamResource/Pages/ListTeams.php
+++ app-modules/Admin/src/Filament/Resources/TeamResource/Pages/ListTeams.php
@@ -1,14 +1,12 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Repwise\Admin\Filament\Resources\TeamResource\Pages;
 
+use Repwise\Admin\Filament\Resources\TeamResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
-use Repwise\Admin\Filament\Resources\TeamResource;
 
-final class ListTeams extends ListRecords
+class ListTeams extends ListRecords
 {
     protected static string $resource = TeamResource::class;
 
