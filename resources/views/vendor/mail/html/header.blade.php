ee96438e4698788e18b691f0f9def7929cfce4d6
Repwise <teddyoweh@gmail.com>
2025-05-17 23:34:01 +0400

    Add email template components: create button, header, footer, layout, message, panel, subcopy, and table blade files

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img src="{{ asset('images/repwise-logo.svg') }}" class="logo" alt="Repwise Logo">
+        </a>
+    </td>
+</tr>
