b83f950c6c4c0eb99a4647a9697f91c0b9ed5024
Repwise <teddyoweh@gmail.com>
2025-05-19 14:35:16 +0400

    Update logo dimensions and change logo image source in header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ asset('images/repwise-logo.svg') }}" class="logo" alt="Repwise Logo">
+            <img src="{{ asset('images/repwise-logo-white.png') }}" class="logo" alt="Repwise Logo">
         </a>
     </td>
 </tr>
