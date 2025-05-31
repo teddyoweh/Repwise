ff822496c298216396d725be6d921fd8fecacf6d
Repwise <teddyoweh@gmail.com>
2025-05-31 23:34:57 +0400

    Add HasTeam trait to Note, Opportunity, and People models for team relationship management

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -26,11 +26,11 @@ use Repwise\CustomFields\Models\Contracts\HasCustomFields;
 final class Note extends Model implements HasCustomFields
 {
     use HasCreator;
+
     /** @use HasFactory<NoteFactory> */
     use HasFactory;
 
     use HasTeam;
-
     use SoftDeletes;
     use UsesCustomFields;
 
